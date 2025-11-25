ee69c2cbcc0c3b11a545d9289dd0ebec4b3fddab
Rahmatdhanid <rahmatdhani2004@gmail.com>
2025-11-25 21:10:45 +0700

    API

--- app/src/main/java/com/akshar/moviereviews/Utils/Constants.java
+++ app/src/main/java/com/akshar/moviereviews/Utils/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
     public static String discoverUrl = "https://api.themoviedb.org/3/search/movie";
     public static String imageUrl = "https://image.tmdb.org/t/p/w500";
     public static String searchUrl = "https://api.themoviedb.org/3/search/movie";
-    public static String api_key = "replace_with_your_api_key";
+    public static String api_key = "8d338c9f5fdf1bcc763cca99b098c429";
 
 }
